52ca7c263917292a8d57aee42309ae5b1d338e0c
Callum <callumbrowne.is@gmail.com>
2018-10-19 23:24:01 +1300

    Removed Build folder

--- build/vendor/glm/glmConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(GLM_VERSION "0.9.7")
-set(GLM_INCLUDE_DIRS "C:/Users/Callum/OneDrive/Uni/Year 3/Tri 2/CGRA 350/Assignments/Final Project/SolarSystemSim/work/vendor/glm")
-
-if (NOT CMAKE_VERSION VERSION_LESS "3.0")
-    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
-endif()
--- build/vendor/glm/test/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Callum/OneDrive/Uni/Year 3/Tri 2/CGRA 350/Assignments/Final Project/SolarSystemSim/work/vendor/glm/test/core
-# Build directory: C:/Users/Callum/OneDrive/Uni/Year 3/Tri 2/CGRA 350/Assignments/Final Project/SolarSystemSim/build/vendor/glm/test/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
