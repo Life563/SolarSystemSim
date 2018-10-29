0f8213072f6449de4393501d6d6ec56a81f901f6
Callum <callumbrowne.is@gmail.com>
2018-10-29 20:34:47 +1300

    Fix

--- work/src/Planet.cpp
+++ work/src/Planet.cpp
@@ -18,25 +18,21 @@
 
 
 /*
-	Use the procesors cycles in order to generate the initial random seed instead of time
+	Use the procesors cycles in order to generate the initial random seed instead of time. Since it has to run on both 
+	ECS and Windows, use this code to check if the platform we are on is windows, and if so import the intrin library. 
 */
 //  Windows
 #ifdef _WIN32
-
-#include <intrin.h>
-uint64_t rdtsc() {
-	return __rdtsc();
-}
-
-//  Linux/GCC
-#else
-
-uint64_t rdtsc() {
-	unsigned int lo, hi;
-	__asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
-	return ((uint64_t)hi << 32) | lo;
-}
-
+	#include <intrin.h>
+	uint64_t rdtsc() {
+		return __rdtsc();
+	}
+#else //  Linux/GCC
+	uint64_t rdtsc() {
+		unsigned int lo, hi;
+		__asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+		return ((uint64_t)hi << 32) | lo;
+	}
 #endif
 
 /**
